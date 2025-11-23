0953d57aea9a9d60b85724b34847c16d813cb8fe
Kantemir <kan.shandir94@gmail.com>
2025-11-23 21:49:56 +0300

    changed url in bot

--- bot/markups.py
+++ bot/markups.py
@@ -3,6 +3,6 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 launch_kb = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(
         text="Launch QuestWalk",
-        web_app=WebAppInfo(url="https://questwalk.vercel.app/")
+        web_app=WebAppInfo(url="https://create-react-app-rust-seven-80.vercel.app/")
     )]
 ])
