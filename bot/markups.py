bddfd4f4fdf1e3f0bf01595073d09974911e264d
Kantemir <kan.shandir94@gmail.com>
2026-02-23 21:29:29 +0300

    fix name

--- bot/markups.py
+++ bot/markups.py
@@ -2,7 +2,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 
 launch_kb = InlineKeyboardMarkup(inline_keyboard=[
     [InlineKeyboardButton(
-        text="Launch QuestWalk",
+        text="Launch GymHero",
         web_app=WebAppInfo(url="https://create-react-app-rust-seven-80.vercel.app/")
     )]
 ])
